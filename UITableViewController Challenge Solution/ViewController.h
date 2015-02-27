2777800d827527c209c8cc48ea45109ec5e4ca26
Fisnik Fejzullahu <user@users-MacBook-Pro-2.local>
2015-02-27 12:55:45 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Fisnik Fejzullahu on 2/27/15.
+//  Copyright (c) 2015 FisFej. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
